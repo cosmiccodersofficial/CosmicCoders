92f4f35de100671fa78e9da6ad9d725f4e3c7241
Manas Bajpai <manas.bajpai16@gmail.com>
2023-11-10 23:45:13 +0530

    Created OurTeam Component

--- src/components/Our Team/OurTeam.js
+++ src/components/Our Team/OurTeam.js
@@ -2,30 +2,27 @@ import React from 'react'
 // import team from '../../images/Our Team.png'
 const OurTeam = () => {
   return (
-    <div className="relative h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: 'url("../../images/Our Team.png")' }}>
-      <div className="absolute inset-0 grid grid-cols-3 grid-rows-3 gap-4">
-        {/* Row 1 */}
-        <div className="row-span-1 col-span-3 flex justify-center items-center text-white text-2xl font-bold">
-          Row 1
-        </div>
-        {/* Row 2 */}
-        sdfjhdsjfhs
-        <div className="row-span-1 col-span-1 flex justify-center items-center text-white text-2xl font-bold">
-          Row 2, Column 1
-        </div>
-        <div className="row-span-1 col-span-1 flex justify-center items-center text-white text-2xl font-bold">
-          Row 2, Column 2
-        </div>
-        <div className="row-span-1 col-span-1 flex justify-center items-center text-white text-2xl font-bold">
-          Row 2, Column 3
-        </div>
-        {/* Row 3 */}
-        <div className="row-span-1 col-span-2 flex justify-center items-center text-white text-2xl font-bold">
-          Row 3, Column 1
-        </div>
-        <div className="row-span-1 col-span-1 flex justify-center items-center text-white text-2xl font-bold">
-          Row 3, Column 2
-        </div>
+    <div className="flex flex-col h-screen pt-16 mt-20">
+      <h1 className='text-3xl pb-10 font-semibold'>Our Team</h1>
+      {/* First Row */}
+      <div className="flex">
+        <div className="flex-1 bg-gray-300 p-4 m-2 rounded-full">Column 1</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 2</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 3</div>
+      </div>
+
+      {/* Second Row */}
+      <div className="flex">
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 4</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 5</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 6</div>
+      </div>
+
+      {/* Third Row */}
+      <div className="flex">
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 7</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 8</div>
+        <div className="flex-1 bg-gray-300 p-4 m-2">Column 9</div>
       </div>
     </div>
   )
