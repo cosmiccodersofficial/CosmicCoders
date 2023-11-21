565849459f12d5a87f8a9821552d2dd4b3bb01ea
Abhay31 <abhaysharma0983@gmail.com>
2023-11-22 00:41:18 +0530

    Updated our team component

--- src/components/Our Team/OurTeam.js
+++ src/components/Our Team/OurTeam.js
@@ -18,7 +18,7 @@ const OurTeam = () => {
     }, []);
     return (
         <div className="bg-gray-50 p-8">
-            <h2 className="text-6xl p-6 font-bold mb-6 mt-28 text-blue-900 animate-slide-in-from-bottom contact-heading">Our Team</h2>
+            <h2 className="text-6xl p-6 font-bold mb-6 mt-28 text-blue-900 contact-heading animate-fade">Our Team</h2>
             {/* First Row */}
             <div className="flex flex-col md:flex-row mb-4 mr-20 ml-20">
                 {/* First Column */}
@@ -27,10 +27,10 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={harshit} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in text-2xl mt-4'> Harshit Sharma </h1>
-                        <h1 className='animate-slide-in text-xl'>Co-Founder &</h1>
-                        <h1 className='animate-slide-in text-xl'>Executive Head</h1>
-                        <Link to="https://www.linkedin.com/in/harshit1822/" target='_blank' className='animate font-semibold'>
+                        <h1 className='text-2xl mt-4 animate-fade'> Harshit Sharma </h1>
+                        <h1 className='text-xl animate-fade'>Co-Founder &</h1>
+                        <h1 className='text-xl animate-fade'>Executive Head</h1>
+                        <Link to="https://www.linkedin.com/in/harshit1822/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -42,10 +42,10 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={archit} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in text-2xl mt-4'>Archit Mishra </h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Co-Founder &</h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Promotional Head</h1>
-                        <Link to="https://www.linkedin.com/in/archit-mishr/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Archit Mishra </h1>
+                        <h1 className='animate-fade text-xl'>Co-Founder &</h1>
+                        <h1 className='animate-fade text-xl'>Promotional Head</h1>
+                        <Link to="https://www.linkedin.com/in/archit-mishr/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -56,10 +56,10 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={manas} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in-from-right text-2xl mt-4'>Manas Bajpai </h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Co-Founder &</h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Tech Head</h1>
-                        <Link to="https://www.linkedin.com/in/manasbajpai/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Manas Bajpai </h1>
+                        <h1 className='animate-fade text-xl'>Co-Founder &</h1>
+                        <h1 className='animate-fade text-xl'>Tech Head</h1>
+                        <Link to="https://www.linkedin.com/in/manasbajpai/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -76,9 +76,9 @@ const OurTeam = () => {
                             {/* Image centered above the h3 tag */}
                             <img src={ayushi} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in text-2xl mt-4'>Ayushi Abrol </h1>
-                        <h1 className='animate-slide-in text-xl'>Operational Head</h1>
-                        <Link to="https://www.linkedin.com/in/ayushi-abrol/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Ayushi Abrol </h1>
+                        <h1 className='animate-fade text-xl'>Operational Head</h1>
+                        <Link to="https://www.linkedin.com/in/ayushi-abrol/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -90,10 +90,10 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={abhay} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in text-2xl mt-4'>Abhay Sharma </h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Community Engagement</h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Manager</h1>
-                        <Link to="https://www.linkedin.com/in/abhaysharma31/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Abhay Sharma </h1>
+                        <h1 className='animate-fade text-xl'>Community Engagement</h1>
+                        <h1 className='animate-fade text-xl'>Manager</h1>
+                        <Link to="https://www.linkedin.com/in/abhaysharma31/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -105,10 +105,10 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={ayush} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in-from-right text-2xl mt-4'>Ayush Dev </h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Operations &</h1>
-                        <h1 className='animate-slide-in-from-right text-xl'>Event Coordinator</h1>
-                        <Link to="https://www.linkedin.com/in/ayush-dev-1a5370211/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Ayush Dev </h1>
+                        <h1 className='animate-fade text-xl'>Operations &</h1>
+                        <h1 className='animate-fade text-xl'>Event Coordinator</h1>
+                        <Link to="https://www.linkedin.com/in/ayush-dev-1a5370211/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
@@ -123,9 +123,9 @@ const OurTeam = () => {
                         <div className="flex flex-col items-center mb-4">
                             <img src={priyanka} alt="" className="w-56 h-56 rounded-full hover cursor-pointer" />
                         </div>
-                        <h1 className='animate-slide-in text-2xl mt-4'>Priyanka Loura</h1>
-                        <h1 className='animate-slide-in text-xl'>YouTube Partner</h1>
-                        <Link to="https://www.linkedin.com/in/priyankaloura/" target='_blank' className='animate font-semibold'>
+                        <h1 className='animate-fade text-2xl mt-4'>Priyanka Loura</h1>
+                        <h1 className='animate-fade text-xl'>YouTube Partner</h1>
+                        <Link to="https://www.linkedin.com/in/priyankaloura/" target='_blank' className='animate-fade animate font-semibold'>
                             Lets Connect <FontAwesomeIcon className='icon-color' icon={faCircleArrowRight} />
                         </Link>
                     </div>
