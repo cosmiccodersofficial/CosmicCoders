a5a9ac82c7e7b673b62fb8a1f680e04b6bfabaee
Abhay31 <abhaysharma0983@gmail.com>
2023-11-28 22:06:34 +0530

    Updated our team

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import JoinUs from './components/JoinUs/JoinUs';
-import Navbar from './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
 import Test from './components/Navbar/Test';
-import OurTeam from './components/Our Team/OurTeam';
+// import OurTeam from './components/Our Team/OurTeam';
+import Test1 from './components/Our Team/Test1';
 import {Routes, Route } from "react-router-dom";
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/ourteam" element={<OurTeam/>}/>
+          <Route exact path="/ourteam" element={<Test1/>}/>
           <Route exact path="/contact" element={<Contact/>}/>
           <Route exact path="/join" element={<JoinUs/>}/>
         </Routes>
