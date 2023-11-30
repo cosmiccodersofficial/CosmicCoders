15e618c0db48edd3643b95cc85556f6a8706e55d
Abhay31 <abhaysharma0983@gmail.com>
2023-11-30 16:26:59 +0530

    updated our team

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Home from './components/Home/Home';
 import Test2 from './components/JoinUs/Test2';
 // import Navbar from './components/Navbar/Navbar';
 import Test from './components/Navbar/Test';
-// import OurTeam from './components/Our Team/OurTeam';
-import Test1 from './components/Our Team/Test1';
+import OurTeam from './components/Our Team/OurTeam';
 import {Routes, Route } from "react-router-dom";
 
 function App() {
@@ -18,7 +17,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/about" element={<About/>}/>
-          <Route exact path="/ourteam" element={<Test1/>}/>
+          <Route exact path="/ourteam" element={<OurTeam/>}/>
           <Route exact path="/contact" element={<Contact/>}/>
           <Route exact path="/join" element={<Test2/>}/>
         </Routes>
