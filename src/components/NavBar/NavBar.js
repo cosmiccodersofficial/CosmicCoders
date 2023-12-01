3f8a5e0ce3f7b88512d1db5b4e2890f33225d862
Manas Bajpai <manas.bajpai16@gmail.com>
2023-12-01 16:08:19 +0530

    Fixed bugs in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -34,7 +34,7 @@ const NavBar = () => {
             <span className={`${!expanded ? 'bar' : 'btn-close btn-close-dark'}`}></span>
             <span className={`${!expanded ? 'bar' : 'd-none '}`}></span>
           </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end mt-2">
+          <Navbar.Collapse id="basic-navbar-nav" className={`justify-content-end ${!expanded ? '' : "mt-2"}`}>
             <Nav onClick={() => setExpanded(false)} className={`cursor-pointer d-flex hover bg-gray-400 align-items-center ${!expanded ? 'rounded-5' : 'rounded-2'} column-gap-4 shadow nav-item p-2 mx-auto`}>
               <Nav.Link as={Link} to="/" className="text-gray-900 fw-bold">
                 <svg className="fill-current h-5 w-5 me-2 bi bi-house-door-fill" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
@@ -46,10 +46,10 @@ const NavBar = () => {
               <Nav.Link as={Link} to="/ourteam" className="text-gray-900 fw-bold"> Our Team </Nav.Link>
               <Nav.Link as={Link} to="/contact" className="text-gray-900 fw-bold"> Contact </Nav.Link>
             </Nav>
-            <Nav className='mt-2' onClick={() => setExpanded(false)}>
+            <Nav className={`${!expanded ? '' : 'mt-2'}`} onClick={() => setExpanded(false)}>
               <Link to="/join" className="text-light fw-bold">
                 <div className={`btn p-2 cursor-pointer shadow-lg hover text-xl ${!expanded ? 'rounded-5' : 'w-100 rounded-2'} bg-blue-900 `}>
-                  <span className="text-white fw-bold text-lg px-1">  Join Us </span>
+                  <span className="text-white fw-bold text-lg px-2">  Join Us </span>
                 </div>
               </Link>
             </Nav>
